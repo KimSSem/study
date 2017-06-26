0eb94ef307822ba016a5b3dd1f6e036e2db4db98
kim <kim@naver.com>
2017-06-26 17:02:29 +0900

    hello.java modified on local repository

--- hello.java
+++ hello.java
@@ -1,4 +1,7 @@
-public class Hello{
+//For command line git tutorial
+//Local repository modification
+
+public class Helloo{
 
   public static void main(String[] args){
      System.out.println("Hello World");
